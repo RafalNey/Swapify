ce949199cf49fa3f8cd5a65c90816cc272f394bc
LJKT1 <94111910+LJKT1@users.noreply.github.com>
2022-02-23 12:05:56 +0000

    Logged in users can send messages

--- components/Messages.jsx
+++ components/Messages.jsx
@@ -16,9 +16,6 @@ import {
 import { signOut } from 'firebase/auth';
 import {auth, database} from '../firebase'
 
-
-
-
 export default function Messages({navigation}) {
   const [messages, setMessages] = useState([]);
 
@@ -41,5 +38,48 @@ export default function Messages({navigation}) {
     });
   }, [navigation]);
 
-  return <GiftedChat />;
+  useLayoutEffect(() => {
+    const collectionRef = collection(database, 'chats');
+    const q = query(collectionRef, orderBy('createdAt', 'desc'));
+
+    const unsubscribe = onSnapshot(q, querySnapshot => {
+      setMessages(
+        querySnapshot.docs.map(doc => ({
+          _id: doc.data()._id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user
+        }))
+      );
+    });
+
+return unsubscribe;
+  });
+
+
+const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages)
+    );
+    const { _id, createdAt, text, user } = messages[0];    
+    addDoc(collection(database, 'chats'), {
+      _id,
+      createdAt,
+      text,
+      user
+    });
+  }, []);
+
+
+  return (
+    <GiftedChat
+      messages={messages}
+      showAvatarForEveryMessage={true}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: auth?.currentUser?.email,
+        avatar: 'https://i.pravatar.cc/300'
+      }}
+    />
+  );
 }
