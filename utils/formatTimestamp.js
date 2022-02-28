f1c731af65f692cfe8e0bb492fb6442dee66d0f8
Nesh00 <c.nenad00@gmail.com>
2022-02-28 16:01:21 +0000

    refactor formatTimestamp

--- utils/formatTimestamp.js
+++ utils/formatTimestamp.js
@@ -1,12 +1,12 @@
 const formattedTimestamp = (timestamp) => {
-    let formattedTimestamp = '';
-    if (typeof timestamp === 'number') {
-      formattedTimestamp = new Date(timestamp);
-    } else {
-      formattedTimestamp = new Date(timestamp.seconds*1000);
-    }
-    const formatted = String(formattedTimestamp);
-    return formatted;
-} 
+  let formattedTimestamp = '';
+  if (typeof timestamp === 'number') {
+    formattedTimestamp = new Date(timestamp);
+  } else {
+    formattedTimestamp = new Date(timestamp.seconds * 1000);
+  }
+  const formatted = String(formattedTimestamp);
+  return formatted.slice(0, formatted.lastIndexOf(':'));
+};
 
 export default formattedTimestamp;
