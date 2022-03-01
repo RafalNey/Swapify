239b7f03a146bf31cc242d0be5cb096ee11f5932
rvallely <68432429+rvallely@users.noreply.github.com>
2022-03-01 17:17:52 +0000

    fix merge conflict bug

--- components/AverageStarRating.jsx
+++ components/AverageStarRating.jsx
@@ -50,7 +50,6 @@ const styles = StyleSheet.create({
         fontSize: 14
     }, 
     customRatingBarStyle: {
-        justifyContent: 'left',
         flexDirection: 'row', 
         flex: 1,
         padding: 0
@@ -59,6 +58,7 @@ const styles = StyleSheet.create({
         width: 20, 
         height: 20, 
         resizeMode: 'cover',
+        
     }, 
     buttonStyle: {
         justifyContent: 'center', 
