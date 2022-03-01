11f68dab5010d4f576cc214df06fe518a4e18cde
Nesh00 <c.nenad00@gmail.com>
2022-03-01 15:49:24 +0000

    when deleting user's account, deletes the user's listed items as well

--- utils/deleteItem.js
+++ utils/deleteItem.js
@@ -1,8 +1,4 @@
-import { removeAssetsFromAlbumAsync } from 'expo-media-library';
 import { deleteDoc, doc } from 'firebase/firestore';
-import { connectStorageEmulator } from 'firebase/storage';
-import { useState } from 'react';
-import { useSafeAreaFrame } from 'react-native-safe-area-context';
 import { database } from '../firebase';
 
 const deleteItem = async (id) => {
