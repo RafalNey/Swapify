dc1cb209038753bdc6b2d03ce16474c851e3656b
Rayhaan Ugharadar <Rayhaan@Mohameds-MacBook-Pro.local>
2022-03-02 23:28:30 +0000

    created list of locations for demo

---
+++ utils/locationList.js
@@ -0,0 +1,89 @@
+export default {
+  Birmingham: {
+    latitude: 52.479853,
+    longitude: -1.901666,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    marker: {
+        latitude: 52.459853,
+        longitude: -1.921666,
+    }
+  },
+
+  Manchester: {
+    latitude: 53.478852,
+    longitude: -2.253916,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    marker: {
+        latitude: 53.478852,
+        longitude: -2.253916,
+    }
+  },
+
+  London: {
+    latitude: 51.503614,
+    longitude: -0.113634,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    marker: {
+        latitude: 51.523614,
+        longitude: -0.113634,
+    }
+  },
+
+  Sheffield: {
+    latitude: 53.381178,
+    longitude: -1.480066,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    marker: {
+        latitude: 53.381178,
+        longitude: -1.480066,
+    }
+  },
+
+  Leeds: {
+    latitude: 53.795886,
+    longitude: -1.550447,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    marker: {
+        latitude: 53.795886,
+        longitude: -1.550447,
+    }
+  },
+
+  Liverpool: {
+    latitude: 53.400219,
+    longitude: -2.98038,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    marker: {
+        latitude: 53.400219,
+        longitude: -2.98038,
+    }
+  },
+
+  Newcastle: {
+    latitude: 54.97318,
+    longitude: -1.621513,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    marker: {
+        latitude: 54.97318,
+        longitude: -1.621513,
+    }
+  },
+
+  Oxford: {
+    latitude: 51.749458,
+    longitude: -1.24454,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    marker: {
+        latitude: 51.749458,
+        longitude: -1.24454,
+    }
+  },
+};
