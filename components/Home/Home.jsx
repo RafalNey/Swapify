c28d52ec6e4f8e2700fb17643e7beb6dd0423d12
Rayhaan Ugharadar <Rayhaan@Mohameds-MacBook-Pro.local>
2022-03-03 11:21:29 +0000

    added welcome message on home page and styled

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet } from 'react-native';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, View, Text } from 'react-native';
 import Logo from './Logo';
 import ItemsSlider from './ItemsSlider';
 import HomeVideo from './HomeVideo';
@@ -12,8 +12,17 @@ const Home = () => {
       <ScrollView>
         <StatusBar default />
         <Logo />
+
+        <View style ={styles.welcomeMessage}>
+          <Text style={styles.welcomeText}>Get rid of unwanted stuff and we won't charge you a penny!</Text>
+          <Text></Text>
+          <Text style={styles.welcomeText}>Done with it? SWAPIFY it!</Text>
+          <Text style={styles.welcomeText}>Get started today!</Text>
+        </View>
         <HomeVideo navigation={navigation} />
+        <View style={styles.sliderContainer}>
         <ItemsSlider />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -24,6 +33,28 @@ const styles = StyleSheet.create({
   homeContainer: {
     flex: 1,
     paddingVertical: '5%',
-    backgroundColor: '#fff',
+    backgroundColor: '#D1D1D1',
   },
+
+  welcomeMessage: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+    margin: 10,
+    borderRadius: 30,
+  },
+
+  welcomeText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
+    
+  },
+
+  sliderContainer: {
+    backgroundColor: '#fff',
+    padding: 15,
+    margin: 10,
+    borderRadius: 30,
+  }
 });
